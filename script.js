ad2b45f5b98b8ce4d226b3f41425814ffd146cd4
erickarodoom <erickarodom@gmail.com>
2023-12-05 20:20:33 -0800

    scafolding work, initial testing out ideas

---
+++ script.js
@@ -0,0 +1,15 @@
+let num;
+let nextNum;
+
+let collection = document.getElementById('calculator');
+console.log(parseFloat(collection.elements.one.value));
+
+function selectedButton(){
+    
+}
+
+
+
+
+
+
