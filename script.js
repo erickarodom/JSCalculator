72723781722a6ce97101538850655da013ead7b9
erickarodoom <erickarodom@gmail.com>
2023-12-07 13:58:46 -0800

    working on logic

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-let num;
-let nextNum;
+let num = "";
+let nextNum= "";
+let numBtn = document.querySelectorAll('.number-btn');
 
-let collection = document.getElementById('calculator');
-console.log(parseFloat(collection.elements.one.value));
+let calculator = document.getElementById('calculator');
+// console.log(parseFloat(collection.elements.one.value));
 
-function selectedButton(){
-    
-}
-
+const displayScreen = document.getElementById('displayTotal');
 
 
+
+calculator.addEventListener('click', function(e){
+    let currentNumber = parseFloat(e.target.value);
+
+    if(currentNumber == NaN ){
+    console.log('select a number');
+    } else {
+    num+= currentNumber;
+    displayScreen.innerText= parseFloat(num);
+    console.log(currentNumber);
+    }
+});
 
 
 
