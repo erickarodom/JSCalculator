ea24a745606a95eb58e04ee49f7c6ebf4f0efd80
erickarodoom <erickarodom@gmail.com>
2023-12-07 14:29:34 -0800

    logic

--- script.js
+++ script.js
@@ -11,14 +11,9 @@ const displayScreen = document.getElementById('displayTotal');
 
 calculator.addEventListener('click', function(e){
     let currentNumber = parseFloat(e.target.value);
-
-    if(currentNumber == NaN ){
-    console.log('select a number');
-    } else {
     num+= currentNumber;
     displayScreen.innerText= parseFloat(num);
     console.log(currentNumber);
-    }
 });
 
 
