6c5c9cf58dda91d6f1be7f30086502843b9d2cc6
erickarodoom <erickarodom@gmail.com>
2023-12-08 15:53:40 -0800

    got NaN if statement to work

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
 let num = "";
 let nextNum= "";
+let symbols = "";
 let numBtn = document.querySelectorAll('.number-btn');
 
-let calculator = document.getElementById('calculator');
+const calculator = document.getElementById('calculator');
 // console.log(parseFloat(collection.elements.one.value));
 
 const displayScreen = document.getElementById('displayTotal');
@@ -11,10 +13,22 @@ const displayScreen = document.getElementById('displayTotal');
 
 calculator.addEventListener('click', function(e){
     let currentNumber = parseFloat(e.target.value);
-    num+= currentNumber;
-    displayScreen.innerText= parseFloat(num);
-    console.log(currentNumber);
+    if(Number.isNaN(currentNumber)){
+        console.log(`The last button you pressed is not a number, the current number is ${num}`);
+       
+    } else {
+        num += currentNumber;
+        console.log(num);
+        displayScreen.innerText = num;
+    }
+    
 });
+
+function theMath(){
+
+}
+
+
 
 
 
