9c40e7e7b5b9a492bc9872145fd13c20ff04f20d
erickarodoom <erickarodom@gmail.com>
2023-12-08 17:00:54 -0800

    continued work on logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-let num = "";
-let nextNum= "";
+let num1 = "";
+let num2= "";
 let symbols = "";
 let numBtn = document.querySelectorAll('.number-btn');
 
@@ -13,15 +13,17 @@ const displayScreen = document.getElementById('displayTotal');
 
 calculator.addEventListener('click', function(e){
     let currentNumber = parseFloat(e.target.value);
-    if(Number.isNaN(currentNumber)){
-        console.log(`The last button you pressed is not a number, the current number is ${num}`);
-       
-    } else {
-        num += currentNumber;
-        console.log(num);
-        displayScreen.innerText = num;
-    }
     
+    if(Number.isNaN(currentNumber)){
+        console.log(`The last button you pressed is not a number, the current number is ${num1}`);
+        let captureNumber = parseInt(num1);
+        console.log(captureNumber);
+        
+    } else {
+        num1 += currentNumber;
+        console.log(num1);
+        displayScreen.innerText = num1;
+    }
 });
 
 function theMath(){
