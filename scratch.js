7fc0316c5865ea554d6e3d9deaa8bc6772769c84
erickarodoom <erickarodom@gmail.com>
2023-12-11 10:24:34 -0800

    add scratch file for notes

---
+++ scratch.js
@@ -0,0 +1,18 @@
+//  TEST CASE
+//  Description: Adding 1 + 2 = 3
+
+// 1.) Click on the number one button
+// Expectation: Show 1 in the display
+// Actual: Works
+
+// 2.) Click on the plus button
+// Expectation: Nothing (Still showing 1 in the display)
+// Actual: Works
+
+// 3.) Click on the number two button
+// Expectation: Show 2 in the display
+// Actual: 12 shows in the display
+
+// 4.) Click on the equals button
+// Expectation: Show 3 in the display
+// Actual: 12 shows in the display
