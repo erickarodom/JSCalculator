7063b01e63d34390aeeecee43f27fe37047fcb6d
erickarodoom <erickarodom@gmail.com>
2024-01-01 08:53:52 -0800

    all work in December

--- script.js
+++ script.js
@@ -5,30 +5,34 @@ let symbols = "";
 let numBtn = document.querySelectorAll('.number-btn');
 
 const calculator = document.getElementById('calculator');
-// console.log(parseFloat(collection.elements.one.value));
 
 const displayScreen = document.getElementById('displayTotal');
 
 
 
 calculator.addEventListener('click', function(e){
-    let currentNumber = parseFloat(e.target.value);
+    let currentNumber = e.target.value;
+    currentNumber = parseInt(currentNumber);
     
-    if(Number.isNaN(currentNumber)){
-        console.log(`The last button you pressed is not a number, the current number is ${num1}`);
-        let captureNumber = parseInt(num1);
-        console.log(captureNumber);
-        
+    if(Number.isNaN(currentNumber) && num1 !== " "){     
+        num1 = parseFloat(num1);
+        console.log(num1);
+        console.log(currentNumber.toString());   
     } else {
+        console.log(currentNumber);
         num1 += currentNumber;
         console.log(num1);
-        displayScreen.innerText = num1;
+        displayScreen.innerText = num1;   
     }
+
 });
 
 function add(a,b){
     return a + b;
+}
 
+function clearDisplay(){
+    document.getElementById('display-total').value = " ";
 }
 
 
