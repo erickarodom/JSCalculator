78a8472475b3afd1f319fe5a183ed551d87a08e3
erickarodoom <erickarodom@gmail.com>
2024-01-02 09:05:03 -0800

    working on calculation issues

--- script.js
+++ script.js
@@ -1,67 +1,90 @@
 //variables=======================
 let num1 = "";
-let num2= "";
-let currentOperator1 = "";
-let priorOperator2 = "";
+let num2 = "";
+let operatorOne = "";
+let operatorTwo = "";
 
-const calculator = document.getElementById('calculator-body');
 const displayScreen = document.getElementById('displayTotal');
 const clearAllBtn = document.querySelector('#clearAll');
-const calculateTotal = document.querySelector('#equalBtn');
+const equalityBtn = document.querySelector('#equalBtn');
 const getNumbers = document.querySelectorAll('.number-btn');
 const getOperators = document.querySelectorAll('.operator');
 
 
 //functions=====================
-getNumbers.forEach((number) => {
+
+  getNumbers.forEach((number) => {
     number.addEventListener('click', (event) => {
       number = event.target.value;
-      displayScreen.innerText = number;
+      showUserInput(number);
     })
   })
-// calculator.addEventListener('click', function(e){
-//     let currentNumber = e.target.value;
-//     currentNumber = parseInt(currentNumber);
-    
-//     if(Number.isNaN(currentNumber) && num1 !== " "){     
-//         num1 = parseFloat(num1);
-//         console.log(num1);
-//         console.log(currentNumber.toString());   
-//     } else {
-//         console.log(currentNumber);
-//         num1 += currentNumber;
-//         console.log(num1);
-//         displayScreen.innerText = num1;   
-//     }
-// });
 
-function add(a,b){
-    return a + b;
-}
-function subtract(a,b){
-    return a - b;
-}
-function multiply(a,b){
-    return a * b;
-}
-function divide(a,b){
-    return a / b;
+  getOperators.forEach((operator) => {
+    operator.addEventListener('click', (event) => {
+      operatorOne = event.target.value
+      if (num1 && num2) {
+        calculateTotal()
+      }
+    })
+  })
+
+const showUserInput = (pressedNum) => {
+    operatorTwo = operatorOne
+    if (!operatorOne) {
+        num1 += pressedNum;
+        displayScreen.innerText = num1;
+    }
+    if (operatorOne || num2) {
+        if (!num2) {
+        num2 = num1;
+        num1 = '';
+        }
+        num1 += pressedNum;
+        displayScreen.innerText = num1;
+        return;
+    }
 }
 
-function showDisplay(){
 
+function calculateTotal() {
+
+    let numOne = parseInt(num1)
+    let numTwo = parseInt(num2)
+  
+    if (operatorTwo === '+') {
+        num2 = numOne + numTwo
+      displayTotal()
+    }
+    if (operatorTwo === '-') {
+        num2 = numTwo - numOne
+      displayTotal()
+    }
+    if (operatorTwo === '*') {
+        num2 = numOne * numTwo
+      displayTotal()
+    }
+    if (operatorTwo === '/') {
+        num2 = numTwo / numOne
+      displayTotal()
+    }
+  }
+
+function displayTotal() {
+displayScreen.textContent = parseFloat( num2)
+firstNumber = ''
 }
+
+equalityBtn.addEventListener('click', calculateTotal);
 
 function clearDisplay(){
     displayScreen.innerHTML = " ";
     num1 = "";
     num2 = "";
-    operator1 = "";
-    operator2 = "";
-    currentNumber = "";
+    operatorOne = "";
+    operatorTwo = "";
+  
 }
-
-//calls================================
 
 clearAllBtn.addEventListener('click', clearDisplay);
 
