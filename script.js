a3458c72195bb3b7277339031c8134c5ab24cbf4
erickarodoom <erickarodom@gmail.com>
2024-01-01 17:27:19 -0800

    current work

--- script.js
+++ script.js
@@ -13,22 +13,27 @@ const getOperators = document.querySelectorAll('.operator');
 
 
 //functions=====================
-
-calculator.addEventListener('click', function(e){
-    let currentNumber = e.target.value;
-    currentNumber = parseInt(currentNumber);
+getNumbers.forEach((number) => {
+    number.addEventListener('click', (event) => {
+      number = event.target.value;
+      displayScreen.innerText = number;
+    })
+  })
+// calculator.addEventListener('click', function(e){
+//     let currentNumber = e.target.value;
+//     currentNumber = parseInt(currentNumber);
     
-    if(Number.isNaN(currentNumber) && num1 !== " "){     
-        num1 = parseFloat(num1);
-        console.log(num1);
-        console.log(currentNumber.toString());   
-    } else {
-        console.log(currentNumber);
-        num1 += currentNumber;
-        console.log(num1);
-        displayScreen.innerText = num1;   
-    }
-});
+//     if(Number.isNaN(currentNumber) && num1 !== " "){     
+//         num1 = parseFloat(num1);
+//         console.log(num1);
+//         console.log(currentNumber.toString());   
+//     } else {
+//         console.log(currentNumber);
+//         num1 += currentNumber;
+//         console.log(num1);
+//         displayScreen.innerText = num1;   
+//     }
+// });
 
 function add(a,b){
     return a + b;
@@ -43,7 +48,9 @@ function divide(a,b){
     return a / b;
 }
 
-function 
+function showDisplay(){
+
+}
 
 function clearDisplay(){
     displayScreen.innerHTML = " ";
